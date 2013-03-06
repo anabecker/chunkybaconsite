eae8cd43bbbceccd3fe3b498168b367ddae9b2c0
Ana Becker <anabdesigns@gmail.com>
2013-03-06 13:13:34 -0500

    made tooltips work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,37 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require twitter/bootstrap
-//= require_tree .
+// = require jquery
+// = require jquery_ujs
+// = require twitter/bootstrap
+// = require_tree .
+//
+$('document').ready(function(){
+  // // var newText = $("p").text().split(" ").join("</span> <span>");
+  // //     newText = "<span>" + newText + "</span>";
+ 
+  // $('p').find(":contains('bacon')")
+  //   var newText = $("p").text().split(" ").join("</span> <span>");
+  //   newText = "<span>" + newText + "</span>";
+  //   .css({"font-style":"italic", "font-weight":"bolder"});
+
+var search = $('body').html();
+search = search.replace(/bacon|Bacon|BACON/g, function($1){
+    return('<a href="#" rel="tooltip" data-toggle="tooltip" title="OMGBACON">' + $1 + '</a>');
+});
+
+// (function($) {
+//   var thePage = $("p");
+//   thePage.html(thePage.html().replace(/bacon/ig, '<a href="#" class="tooltip" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip on bottom">bacon</a>')); 
+// });
+
+$('body').html(search);
+$("a[rel=tooltip]").tooltip()
+
+// $('.tooltip').tooltip();
+
+ });
+
+// <a href="#" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip on bottom">Tooltip on bottom</a>
+
+//irregular expression
